a2655d07dfc7938726436e30d402386c62c8e845
Andy Walpole <me@andywalpole.me>
2015-12-20 23:33:26 +0000

    fixed scss formatting issues

--- gulp/gulp-dev/gulp-sass.js
+++ gulp/gulp-dev/gulp-sass.js
@@ -1,8 +1,11 @@
 module.exports = function(gulp, plugins, filePaths) {
   'use strict';
   return function() {
-    gulp.src(filePaths.sassInput + 'main.scss')
+    gulp.src(filePaths.sassInput + '*.scss')
       .pipe(plugins.sourcemaps.init())
+      .pipe(plugins.scssLint({
+        'maxBuffer': Infinity
+      }))
       .pipe(plugins.sass({outputStyle: 'expanded'}).on('error', plugins.sass.logError))
       .pipe(plugins.autoprefixer({
         browsers: ['last 2 Chrome versions'],
