3e7637bbf5f1001835a72a9ed2d1152a344c0a88
Andy Walpole <me@andywalpole.me>
2015-12-20 14:39:31 +0000

    added mpre basic linting configuration

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,26 @@
  * Created by andywalpole on 18/12/2015.
  */
 // sudo npm install -g commitizen
-  // sudo npm install -g conventional-changelog
+// sudo npm install -g conventional-changelog
+// sudo npm install -g modernizr
+//  delete files and folders -> https://github.com/gulpjs/gulp/blob/master/docs/recipes/delete-files-folder.md
+'use strict';
 
-var gulp = require('gulp');
-var conventionalChangelog = require('gulp-conventional-changelog');
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
+
+const devPath = './gulp/gulp-development/';
+const prodPath = './gulp/gulp-production/';
+
+let getDevTask = function getTask(task) {
+  return require(devPath + task)(gulp, plugins);
+};
+
+let getProdTask = function getTask(task) {
+  return require(prodPath + task)(gulp, plugins);
+};
+
+gulp.task('sass', getDevTask('gulp-sass'));
 
 gulp.task('default', function() {
   return gulp.src('CHANGELOG.md', {
