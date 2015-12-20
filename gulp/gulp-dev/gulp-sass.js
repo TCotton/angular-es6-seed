73dd5fb8cf1a03a070eaa8c005349874645f98b4
Andy Walpole <me@andywalpole.me>
2015-12-20 22:29:58 +0000

    changed gulp sass configuration

--- gulp/gulp-dev/gulp-sass.js
+++ gulp/gulp-dev/gulp-sass.js
@@ -3,10 +3,7 @@ module.exports = function(gulp, plugins, filePaths) {
   return function() {
     gulp.src(filePaths.sassInput + 'main.scss')
       .pipe(plugins.sourcemaps.init())
-      .pipe(plugins.sass({
-        outputStyle: 'expanded'
-      }))
-      .pipe(plugins.sass().on('error', plugins.sass.logError))
+      .pipe(plugins.sass({outputStyle: 'expanded'}).on('error', plugins.sass.logError))
       .pipe(plugins.autoprefixer({
         browsers: ['last 2 Chrome versions'],
         cascade: false,
